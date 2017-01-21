87dd435406e8d577634c8d69a7dd03ab636a1d1f
magnuschatt <magnus.chatt@gmail.com>
2017-01-21 16:07:17 +0100

    Hope this works

--- src/main/kotlin/chatt/moment/Application.kt
+++ src/main/kotlin/chatt/moment/Application.kt
@@ -6,6 +6,18 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import lombok.Data
+import lombok.RequiredArgsConstructor
+import org.springframework.beans.factory.annotation.Autowired
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.validation.constraints.NotNull
+import org.springframework.data.repository.CrudRepository
+
+
+
 
 @SpringBootApplication
 class Application
@@ -15,12 +27,30 @@ fun main(args: Array<String>) {
 }
 
 @Controller
-class GreetingController {
+class GreetingController
+@Autowired
+constructor(private val repo: UserRepository) {
 
     @RequestMapping("/greeting")
     fun greeting(@RequestParam(value = "name", required = false, defaultValue = "World") name: String, model: Model): String {
-        model.addAttribute("name", name)
+        model.addAttribute("name", System.getenv("MAGNUS"))
+        model.addAttribute("repoCount", repo.count())
         return "greeting"
     }
 
 }
+
+@Entity
+@Data
+@RequiredArgsConstructor
+class AppUser {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private var id: Long? = null
+
+    @NotNull
+    private var username: String? = null
+
+}
+
+interface UserRepository : CrudRepository<AppUser, Long>
